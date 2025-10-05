07724367681ea5da835fe695a936da4e3be1e0cc
Abhi-4793 <abhinavkrishnapy2@gmail.com>
2025-10-05 17:08:07 +0530

    added folder structure for hooks,components and utils and types and  added types for property and weather and api call for property

---
+++ frontend/warden-frontend/src/libs/api.ts
@@ -0,0 +1,12 @@
+import axios from axios;
+import { Property } from "@/types";
+
+const BACKEND_ENDPOINT = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:5000';
+
+export async function fecthProperties(q?:string,limit=20):Promise<Property[]>{
+    const res = await axios.get(`${BACKEND_ENDPOINT}/get-properties`,{
+        params:{q,limit},
+    });
+    return res.data as Property[];
+
+}
---
+++ frontend/warden-frontend/src/types/index.ts
@@ -0,0 +1,13 @@
+export type Property = {
+  id: number;
+  name: string;
+  address?: string | null;
+  longitude: number;
+  latitude: number;
+  [key: string]: any;
+};
+export type weather = {
+  temprature: number | null;
+  humidity: number | null;
+  weathercode: number | null;
+};
